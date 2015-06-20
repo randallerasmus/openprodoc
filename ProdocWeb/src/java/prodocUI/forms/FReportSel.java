ee98810156d66807c6ee985cf22693a2a3ae8a4d
jhierrot <jhierrot@0dbe52e2-3855-517d-bc6d-d04104cc4623>
2015-06-20 13:06:30 +0000

    Reports working for 1 result

--- ProdocWeb/src/java/prodocUI/forms/FReportSel.java
+++ ProdocWeb/src/java/prodocUI/forms/FReportSel.java
@@ -31,6 +31,7 @@ import prodoc.PDException;
 import prodoc.PDFolders;
 import prodoc.PDReport;
 import prodoc.Record;
+import prodocUI.servlet.GenReport;
 import prodocUI.servlet.SParent;
 import prodocUI.servlet.SendDoc;
 
@@ -72,22 +73,7 @@ Table FormTab=new Table(3, 4, 0);
 FormTab.setCellPadding(10);
 FormTab.setWidth(-100);
 FormTab.setCSSClass("FFormularios");
-NumDocsPage=new FieldText(NumDocsPageName);
-NumDocsPage.setMaxSize(6);
-NumDocsPage.setCSSClass("FFormInput");
-NumDocsPage.setMensStatus(TT("Docs_per_Page"));
-NumPagesFile=new FieldText(NumPagesFileName);
-NumPagesFile.setMaxSize(6);
-NumPagesFile.setCSSClass("FFormInput");
-NumPagesFile.setMensStatus(TT("Pages_per_File"));
-//FormTab.getCelda(0,0).setWidth(-25);
 FormTab.getCelda(0,0).setHeight(30);
-FormTab.getCelda(1,0).AddElem(new Element(TT("Docs_per_Page")+":"));
-FormTab.getCelda(1,0).AddElem(Element.getEspacio2());
-FormTab.getCelda(1,0).AddElem(NumDocsPage);
-FormTab.getCelda(1,1).AddElem(new Element(TT("Pages_per_File")+":"));
-FormTab.getCelda(1,1).AddElem(Element.getEspacio2());
-FormTab.getCelda(1,1).AddElem(NumPagesFile);
 Table TabDocs=new Table(5,1,1);
 TabDocs.setCellSpacing(0);
 TabDocs.setWidth(-100);
@@ -103,8 +89,7 @@ AttrD=NextDoc.getAttr(PDReport.fPAGESDOC);
 TabDocs.getCelda(2,0).AddElem(new Element(TT(AttrD.getUserName())));
 AttrD=NextDoc.getAttr(PDReport.fMIMETYPE);
 TabDocs.getCelda(3,0).AddElem(new Element(TT(AttrD.getUserName())));
-AttrD=NextDoc.getAttr(Rep.fPDDATE);
-TabDocs.getCelda(4,0).AddElem(new Element(TT(AttrD.getUserName())));
+TabDocs.getCelda(4,0).AddElem(new Element(TT("Generate_Report")));
 String ActDoc=SParent.getActDocId(Req);
 String DocId=null;
 Cursor ListDocs=Rep.GetListReports();
@@ -121,41 +106,21 @@ while (NextDoc!=null)
     TabDocs.getCelda(2,Row).AddElem(new Element(((Integer)AttrD.getValue()).toString()));
     AttrD=NextDoc.getAttr(PDReport.fMIMETYPE);
     TabDocs.getCelda(3,Row).AddElem(new Element((String)AttrD.getValue()));
-    AttrD=NextDoc.getAttr(PDReport.fPDDATE);
-    TabDocs.getCelda(4,Row).AddElem(new Element(SParent.FormatTS(Req,(Date)AttrD.getValue())));
     DocId=(String)NextDoc.getAttr(PDDocs.fPDID).getValue();
-    if (DocId.equals(ActDoc))
-        TabDocs.getFila(Row).setCSSClass("ListDocsSel");
-    else
-        TabDocs.getFila(Row).setCSSClass("ListDocs");
-    TabDocs.getFila(Row).setCSSId(DocId);
-    TabDocs.getFila(Row).setOnClick("SelectRow('"+DocId+"')");
+    HiperlinkImag hv=new html.HiperlinkImag("img/"+getStyle()+"Report.png" , "Report", GenReport.getUrlServlet()+"?Type=Fold&Id="+DocId, "");
+    hv.setTarget("_blank");
+    TabDocs.getCelda(4,Row).setAlineacion(Element.CENTER); 
+    TabDocs.getCelda(4,Row).AddElem(hv);
     NextDoc=PDSession.NextRec(ListDocs);
     }
 FormTab.getCelda(1,2).setCSSClass("ListDocs");
 FormTab.getCelda(1,2).AddElem(TabDocs);
 PDSession.CloseCursor(ListDocs);
-FormTab.getCelda(1,3).AddElem(OkButton);
 FormTab.getCelda(1,3).AddElem(CancelButton);
 Form LoginForm=new Form(Destination+"?Read=1","FormVal");
 BorderTab.getCelda(0,1).AddElem(FormTab);
 LoginForm.AddElem(BorderTab);
 AddElem(LoginForm);
-}
-//-----------------------------------------------------------------------------------------------    
-@Override
-protected String getFormHelp()
-{
-switch (Mode)  
-    {
-    case ADDMOD:
-        return("AddFolder");
-    case DELMOD:
-        return("DelFolder");
-    case EDIMOD:
-        return("ModFolder");
-    }
-return("HelpIndex");
 }
 //-----------------------------------------------------------------------------------------------    
 }
